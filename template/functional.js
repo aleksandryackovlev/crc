64badfa1de86a09cf2a2d4061cb2f6983423476b
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2019-05-04 06:32:46 +0300

    Add the template directory

---
+++ template/functional.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// %PROP_TYPES_IMPORT%
+
+// %ADDITIONAL_IMPORT%
+
+%CSS_IMPORT%
+
+const %COMPONENT_NAME% = (%PROPS%) => (
+  <div>
+
+  </div>
+);
+
+// %PROP_TYPES_DEFINITION%
+
+// %DEFAULT_PROPS%
+
+// %CONNECT%
+
+export default %COMPONENT_NAME%;
