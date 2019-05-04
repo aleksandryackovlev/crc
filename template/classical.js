849d99c01e174402a336d389dc9ee9d04dbce8f5
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2019-05-04 14:40:08 +0300

    Add template files for styles and classical components

---
+++ template/classical.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+// %PROP_TYPES_IMPORT%
+
+// %ADDITIONAL_IMPORT%
+
+%CSS_IMPORT%
+
+class %COMPONENT_NAME% extends Component {
+  render() {
+    const %PROPS% = this.props;
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+// %PROP_TYPES_DEFINITION%
+
+// %DEFAULT_PROPS%
+
+// %CONNECT%
+
+export default %COMPONENT_NAME%;
